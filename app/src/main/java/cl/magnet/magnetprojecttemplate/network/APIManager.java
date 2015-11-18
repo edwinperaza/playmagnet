43f3f521eba028ab32a344f4a3fe773a59372bcb
Yaniv <ygoldfrid@gmail.com>
2015-11-18 15:48:48 -0300

    Base URL for Debug and Release

--- app/src/main/java/cl/magnet/magnetprojecttemplate/network/APIManager.java
+++ app/src/main/java/cl/magnet/magnetprojecttemplate/network/APIManager.java
@@ -1,12 +1,14 @@
 package cl.magnet.magnetprojecttemplate.network;
 
+import cl.magnet.magnetprojecttemplate.BuildConfig;
+
 /**
  * Created by yaniv on 10/28/15.
  */
 public class APIManager {
 
     //API Ur
-    protected final static String BASE_URL = "http://test.magnet.cl/";
+    protected final static String BASE_URL = BuildConfig.BASE_URL;
     protected final static String API_URL = "api/v1/";
 
 
