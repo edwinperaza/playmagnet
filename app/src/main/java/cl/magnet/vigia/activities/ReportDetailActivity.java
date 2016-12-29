2398afed7164c70419a4210ae4205cf1e38d8685
edwinperaza <edwinperaza87@gmail.com>
2016-12-29 18:14:25 -0300

    Add Report Detail layout V1 (still need attention)

--- app/src/main/java/cl/magnet/vigia/activities/ReportDetailActivity.java
+++ app/src/main/java/cl/magnet/vigia/activities/ReportDetailActivity.java
@@ -2,15 +2,21 @@ package cl.magnet.vigia.activities;
 
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import cl.magnet.vigia.R;
 
 public class ReportDetailActivity extends AppCompatActivity {
+
+    private ImageView mReportAcceptImageView;
+    private ImageView mReportDeniedImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +29,25 @@ public class ReportDetailActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         final Drawable upArrow = ContextCompat.getDrawable(this, R.drawable.ic_back_arrow);
         getSupportActionBar().setHomeAsUpIndicator(upArrow);
+
+        mReportAcceptImageView = (ImageView) findViewById(R.id.iv_report_accept);
+        mReportDeniedImageView = (ImageView) findViewById(R.id.iv_report_denied);
+
+        mReportAcceptImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Reporte Aceptado", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        mReportDeniedImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Reporte Negado", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
     }
 
